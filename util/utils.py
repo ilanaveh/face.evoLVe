734e9800c3153bc61fcb9fe2f7c2ea6f33391662
ilanaveh <ilana.naveh@mail.huji.ac.il>
2025-12-29 11:25:03 +0200

    Remove bcolz (module doesn't exist in container) + change 'view' to 'reshape' (due to bug with view)

--- util/utils.py
+++ util/utils.py
@@ -9,7 +9,7 @@ import matplotlib.pyplot as plt
 plt.switch_backend('agg')
 import numpy as np
 from PIL import Image
-import bcolz
+# import bcolz
 import io
 import os
 
@@ -249,7 +249,7 @@ def accuracy(output, target, topk=(1,)):
 
     res = []
     for k in topk:
-        correct_k = correct[:k].view(-1).float().sum(0)
+        correct_k = correct[:k].reshape(-1).float().sum(0)
         res.append(correct_k.mul_(100.0 / batch_size))
 
     return res
