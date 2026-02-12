2cdb1665cea0f4e2e54ddb544c53bda588f8649d
ilanaveh <ilana.naveh@mail.huji.ac.il>
2026-02-12 15:03:42 +0200

    Code for getting mean & std rgb values of dataset (for normalizing)

---
+++ get_dataset_stats.py
@@ -0,0 +1,46 @@
+"""
+12/2/26
+For normalization - get mean (& std) rgb of dataset.
+Dataset: MS-Celeb-1M_Align_112x112
+"""
+
+import os
+import numpy as np
+from PIL import Image
+
+
+data_dir = '/home/projects/bagon/ilanaveh/data/imgs'
+
+# Supported image extensions
+IMG_EXTENSIONS = ('.png', '.jpg', '.jpeg', '.bmp', '.tiff', '.webp')
+
+pixel_sum = np.zeros(3)
+pixel_sq_sum = np.zeros(3)
+pixel_count = 0
+img_count = 0
+dir_count = 0
+
+for root, _, files in os.walk(data_dir):
+    dir_count += 1
+    print(f"directory #{dir_count}: {root.split('/')[-1]}")
+    for file in files:
+        if file.lower().endswith(IMG_EXTENSIONS):
+            img_count += 1
+            img_path = os.path.join(root, file)
+
+            # Open image and convert to RGB
+            img = Image.open(img_path).convert('RGB')
+            img = np.array(img) / 255.0  # Normalize to [0,1]
+
+            # Reshape to (num_pixels, 3)
+            img = img.reshape(-1, 3)
+
+            pixel_sum += img.sum(axis=0)
+            pixel_sq_sum += (img ** 2).sum(axis=0)
+            pixel_count += img.shape[0]
+
+mean = pixel_sum / pixel_count
+std = np.sqrt(pixel_sq_sum / pixel_count - mean ** 2)
+
+print("Mean RGB:", mean)
+print("Std RGB:", std)
